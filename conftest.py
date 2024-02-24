e762bbe64c0e349ac4422097710c702f256413b9
YanaKarpova1973 <160159043+YanaKarpova1973@users.noreply.github.com>
2024-02-25 00:23:49 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import pytest
-
-from BookCollector import BooksCollector
+from main import BooksCollector
 
 @pytest.fixture # фикстура, которая создаёт спиок из трех книг
 def three_books():
@@ -15,7 +14,7 @@ def three_books():
     three_books.set_book_genre('10 негритят', 'Детективы')
     return three_books
 
-@pytest.fixture # фикстура, которая создаёт спиок книг для детей
+@pytest.fixture # фикстура, которая создаёт спиcок книг для детей
 def children_books():
     children_books = BooksCollector()
     # добавляем две книги
@@ -25,3 +24,30 @@ def children_books():
     children_books.set_book_genre('Три поросенка', 'Мультфильмы')
     children_books.set_book_genre('Золушка', 'Мультфильмы')
     return children_books
+
+@pytest.fixture # фикстура, которая создаёт спиcок избранных книг
+def favorite_books():
+    favorite_books = BooksCollector()
+
+    # добавляем книгу
+    favorite_books.add_new_book('Чебурашка')
+    favorite_books.add_new_book('Кристина')
+    favorite_books.add_new_book('Сияние')
+    favorite_books.add_new_book('10 негритят')
+    # устанавливаем жанр книг
+    favorite_books.set_book_genre('Чебурашка', 'Мультфильмы')
+    favorite_books.set_book_genre('Кристина', 'Ужасы')
+    favorite_books.set_book_genre('Сияние', 'Ужасы')
+    favorite_books.set_book_genre('10 негритят', 'Детективы')
+    favorite_books.add_book_in_favorites('Чебурашка')
+    favorite_books.add_book_in_favorites('Кристина')
+    favorite_books.add_book_in_favorites('Сияние')
+    favorite_books.add_book_in_favorites('10 негритят')
+    return favorite_books
+
+@pytest.fixture  # фикстура, которая создаёт спиcок избранных книг
+def strange_book():
+    strange_book = BooksCollector()
+    strange_book.books_genre = {'Чебурашка': 'Мультфильмы', 'Кристина': 'Ужасы', 'Сияние': 'Ужасы', '10 негритят': 'Детективы'}
+    strange_book.favorites = ['Чебурашка', 'Кристина', 'Сияние', '10 негритят']
+    return strange_book
