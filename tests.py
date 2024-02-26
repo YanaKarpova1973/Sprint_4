c8cafb3385d6cd37333aefbd96eb50004c899eda
YanaKarpova1973 <160159043+YanaKarpova1973@users.noreply.github.com>
2024-02-26 20:13:54 +0300

    Corrected tests.py

--- tests.py
+++ tests.py
@@ -5,10 +5,11 @@ from main import BooksCollector
 class TestBooksCollector:
 
     # тестируем add_new_book
-    def test_add_new_book_addition_result(self):
+    @pytest.mark.parametrize('book_name',['Книга джунглей'] )
+    def test_add_new_book_addition_result(self, book_name):
         collector = BooksCollector()
-        collector.add_new_book('Книга джунглей')                  # вызываем метод
-        assert collector.books_genre['Книга джунглей'] == ''      # тестируем значение, которое он меняет, поскольку метод ничего не возвращает
+        collector.add_new_book(book_name)
+        assert  book_name in collector.get_books_genre()
 
     #тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
@@ -18,7 +19,7 @@ class TestBooksCollector:
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         # словарь имеет длину 2
-        assert len(collector.books_genre) == 2
+        assert (len(collector.get_books_genre()),2)
 
     @pytest.mark.parametrize('set_genre', ["Мультфильмы"])
     def test_set_book_genre_get_genre(self, set_genre):
@@ -27,7 +28,7 @@ class TestBooksCollector:
         set_book = 'Чебурашка'
         collector.add_new_book(set_book)
         collector.set_book_genre(set_book, set_genre)
-        assert  collector.books_genre == {'Чебурашка': 'Мультфильмы'}
+        assert  set_genre in collector.get_book_genre(set_book)
 
     # получаем жанр книги по её имени
     def test_get_book_genre_using_name(self, three_books):
@@ -35,7 +36,7 @@ class TestBooksCollector:
 
     # выводим список книг с определённым жанром
     def test_get_books_with_specific_genre_horror(self, three_books):
-        assert  'Кристина', 'Сияние' in three_books.get_books_with_specific_genre
+        assert  'Кристина', 'Сияние' in three_books.get_books_with_specific_genre('Ужасы')
 
     # получаем словарь books_genre
     def test_get_books_genre_from_two_books(self):
@@ -51,17 +52,17 @@ class TestBooksCollector:
 
     # возвращаем книги, подходящие детям
     def test_get_books_for_children_set_result(self, children_books):
-        assert (children_books.books_genre[children_books.get_books_for_children()[0]]  == 'Мультфильмы') and (children_books.books_genre[children_books.get_books_for_children()[1]]  == 'Мультфильмы')
+        assert children_books.get_books_for_children()  == ['Три поросенка', 'Золушка']
 
     # добавляем книгу в Избранное
     def test_add_book_in_favorites_create_list(self, three_books):
         three_books.add_book_in_favorites('10 негритят')
-        assert three_books.favorites[0] == '10 негритят'
+        assert '10 негритят' in three_books.get_list_of_favorites_books()
 
     # # удаляем книгу из Избранного
     def test_delete_book_from_favorites_result(self, strange_book):
         strange_book.delete_book_from_favorites('Кристина')
-        assert 'Кристина' not in strange_book.favorites
+        assert 'Кристина' not in strange_book.get_list_of_favorites_books()
 
     # получаем список Избранных книг
     def test_get_list_of_favorites_books_result(self, favorite_books):
